b024a4d182715e8948fbf73537912850fa8b2f76
Anthony Tucker <54104732+ATphobia22@users.noreply.github.com>
2026-09-06 18:53:59 -0500

    chore(engines): export new MedicalNet, ComprehendFHIR, ClinicalLLM modules

--- engines/__init__.py
+++ engines/__init__.py
@@ -3,10 +3,16 @@ from .simulation_compute_mesh import SimulationComputeMesh
 from .monai_vision_node import MONAIVisionNode
 from .precision_medicine_engine import PrecisionMedicineEngine
 from .integrated_ehr_bridge import IntegratedEHRBridge
+from .medicalnet_backbone import MedicalNetBackbone
+from .comprehend_fhir_bridge import ComprehendFHIRBridge
+from .clinical_llm_router import ClinicalLLMRouter
 
 __all__ = [
     "SimulationComputeMesh",
     "MONAIVisionNode",
     "PrecisionMedicineEngine",
     "IntegratedEHRBridge",
+    "MedicalNetBackbone",
+    "ComprehendFHIRBridge",
+    "ClinicalLLMRouter",
 ]
